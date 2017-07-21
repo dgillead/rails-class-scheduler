91110311c0a6b4a6f912c3e78315747744a49704
Darnell Gillead <dlgillead@gmail.com>
2017-07-21 10:40:42 -0500

    added route to display a single studio

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,18 +1,28 @@
 class StudiosController < ApplicationController
   before_action :find_user, only: [:create]
+  before_action :find_studio, only: [:show]
 
   def new
     @studio = Studio.new
   end
 
   def create
-    @studio = Studio.new(studio_params)
-    @studio['user_id'] = current_user.id
-    binding.pry
-    @studio.save!
+    @studio = current_user.studios.new(studio_params)
+    if @studio.save
+      redirect_to @studio
+    else
+      render :new
+    end
+  end
+
+  def show
   end
 
   private
+
+  def find_studio
+    @studio = Studio.find_by(id: params[:id])
+  end
 
   def find_user
     @user = User.find_by(id: params[:id])
